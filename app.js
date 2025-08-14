f1088aa0f7669a389c7ca03e7955671d95460c99
Abdulshakur <abdulshakurmahmood@gmail.com>
2025-08-14 09:37:07 +0100

    added error handler to js

--- app.js
+++ app.js
@@ -6,20 +6,25 @@ const humidityValue = document.querySelector(".humidity-value");
 const windSpeedValue = document.querySelector(".wind-speed-value");
 const searchText = document.querySelector("#search-text");
 const searchBtn = document.querySelector(".search-btn");
-
-// let weatherDescription = switch()
+const weatherBody = document.querySelector(".weather-body");
+const error = document.querySelector(".error");
 
 searchBtn.addEventListener("click", async (e) => {
   if (searchText.value === "") return;
+  weatherBody.style.display = "none";
+  error.style.display = "none";
   try {
     const searchCity = searchText.value.trim().toLowerCase();
+
     const response = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=b910d7bc47455cdda1adc8885a153a38&units=metric`
     );
-    const data = await response.data;
+
+    const data = response.data;
+
+    weatherBody.style.display = "block";
     temp.textContent = Math.ceil(data.main.temp);
     city.textContent = data.name;
-    //
 
     const mainWeather = data.weather[0].main;
     switch (mainWeather) {
@@ -51,7 +56,8 @@ searchBtn.addEventListener("click", async (e) => {
     humidityValue.textContent = Math.ceil(data.main.humidity);
 
     searchText.value = "";
-  } catch (error) {
-    console.log(`Error getting message: ${error}`);
+  } catch (err) {
+    console.log(`Error getting message: ${err}`);
+    error.style.display = "block";
   }
 });
